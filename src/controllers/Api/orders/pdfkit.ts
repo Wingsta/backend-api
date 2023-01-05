e2c971f0e6b9b9109ddb609e186f965c88ce6d90
vishal <jsvishal2014@gmail.com>
2023-01-05 22:10:31 +0530

    add

--- src/controllers/Api/orders/pdfkit.ts
+++ src/controllers/Api/orders/pdfkit.ts
@@ -186,7 +186,7 @@ async function generateTableRow(
     .font("Roboto")
     .fontSize(10)
     .text(item, 50, y)
-    .text(description, 150, y)
+    
     .text(unitCost, 280, y, { width: 90, align: "right" })
     .text(quantity, 370, y, { width: 90, align: "right" })
     .text(lineTotal, 0, y, { align: "right" })
